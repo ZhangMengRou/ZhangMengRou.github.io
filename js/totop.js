d6d3b984c5c3305b8a5254737521db7fe27444a2
ZhangMengRou <2308666855@qq.com>
2017-03-26 15:12:54 +0800

    Site updated: 2017-03-26 15:12:47

--- js/totop.js
+++ js/totop.js
@@ -25,3 +25,4 @@
 		$('html, body').animate({scrollTop:0}, scrollSpeed); return false;
 	});
 })(jQuery);
+var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1261582497'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s4.cnzz.com/z_stat.php%3Fid%3D1261582497' type='text/javascript'%3E%3C/script%3E"));
